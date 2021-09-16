7fd29d059285ab07f2b1c62123c7a0cea07c6d4a
Asril Rinaldi <asrilrinaldi@gmail.com>
2021-09-16 22:05:33 +0700

    tambah controller matakuliah

---
+++ app/Http/Controllers/MatakuliahController.php
@@ -0,0 +1,24 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+ 
+class MatakuliahController extends Controller
+{
+    public function index($nama){
+      return $nama;
+      
+    }
+    public function formulir(){
+ 
+    	return view('formulir');
+ 
+    }
+    public function proses(Request $request){
+        $nama = $request->input('nama');
+     	$alamat = $request->input('alamat');
+        return "Nama : ".$nama.", Alamat : ".$alamat;
+    }
+
+}
