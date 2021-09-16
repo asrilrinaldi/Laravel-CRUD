51b9ba866974fbb619dcf7ff8f43440769a89d44
Asril Rinaldi <asrilrinaldi@gmail.com>
2021-09-16 22:55:18 +0700

    konfigurasi controller

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -3,13 +3,21 @@
 namespace App\Http\Controllers;
  
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
  
 class MahasiswaController extends Controller
 {
-    public function index(){
-      $nama = "Asril Rinaldi";
-      $matkul = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
-    	return view('mahasiswa',['nama' => $nama, 'matkul' => $matkul]);
+    // public function index(){
+    //   $nama = "Asril Rinaldi";
+    //   $matkul = ["Algoritma & Pemrograman","Kalkulus","Pemrograman Web"];
+    // 	return view('mahasiswa',['nama' => $nama, 'matkul' => $matkul]);
       
+    // }
+    public function index(){
+      //ambil data dari tabel mahasiswa
+      $mahasiswa = DB::table('mahasiswa')->get();
+
+      //mengirim data ke view index
+      return view('index',['mahasiswa'=> $mahasiswa]);
     }
 }
